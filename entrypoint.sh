456bf81742a199397b1c9b66cbecf7d458d90b45
Tim Schumacher <timschumi@gmx.de>
2021-11-25 22:59:26 +0100

    Use repo paths from the repository list

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,13 +12,16 @@
 
         > cgitrepos
         while read -r line; do
-            if ! [[ "${line}" =~ *".git" ]]; then
-                line="${line}.git"
+            line_split=(${line// : / })
+            line_path=${line_split[0]}
+            line_url=${line_split[1]}
+            if ! [[ "${line_path}" =~ *".git" ]]; then
+                line_path="${line_path}.git"
             fi
-            echo "repo.url=${line}" >> cgitrepos
-            echo "repo.path=/repos/${line}" >> cgitrepos
+            echo "repo.url=${line_url}" >> cgitrepos
+            echo "repo.path=/repos/${line_path}" >> cgitrepos
             echo "" >> cgitrepos
-        done <<< "$(repo list -p)"
+        done <<< "$(repo list)"
 
         sleep `expr 60 \* ${UPDATE_INTERVAL}`
     done
