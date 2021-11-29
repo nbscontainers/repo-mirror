4f9d5facaee2e36ed4bef5def16ce399fbef77ad
Tim Schumacher <timschumi@gmx.de>
2021-11-29 13:57:28 +0100

    Provide an alternative .git URL

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,6 +23,14 @@
             echo "repo.url=${line_url}" >> cgitrepos
             echo "repo.path=/repos/${line_path}" >> cgitrepos
             echo "" >> cgitrepos
+
+            # Provide an alternative .git URL
+            if ! [[ "${line_url}" =~ ^.*\.git$ ]]; then
+                echo "repo.url=${line_url}.git" >> cgitrepos
+                echo "repo.path=/repos/${line_path}" >> cgitrepos
+                echo "repo.hide=1"  >> cgitrepos
+                echo "" >> cgitrepos
+            fi
         done <<< "$(repo list)"
 
         sleep `expr 60 \* ${UPDATE_INTERVAL}`
