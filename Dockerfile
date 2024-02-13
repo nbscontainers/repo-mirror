c524fb0d83deb849cc762e99b628742ff32ed515
Tim Schumacher <timschumi@gmx.de>
2024-02-13 12:59:53 +0100

    Convert to a one-shot container

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
-FROM ghcr.io/nbscontainers/cgit:latest
+FROM docker.io/alpine:latest
 
 RUN apk add --no-cache python3 curl bash git gnupg openssh && \
     ln -sf python3 /usr/bin/python && \
     curl https://storage.googleapis.com/git-repo-downloads/repo > /usr/local/bin/repo && \
-    chmod a+x /usr/local/bin/repo && \
-    sed -i 's#scan-path=.*$#include=/repos/cgitrepos#g' /etc/cgitrc
+    chmod a+x /usr/local/bin/repo
 
 COPY entrypoint.sh /entrypoint.sh
 
-ENV UPDATE_INTERVAL=60
 ENV MANIFEST_URL=""
 ENV MANIFEST_REVISION="HEAD"
 ENV MANIFEST_NAME="default.xml"
 ENV MANIFEST_GROUPS="default"
+
+VOLUME /repos
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,40 +1,32 @@
-#!/bin/bash -e
+#!/bin/bash -eu
 
-{
-    cd /repos
+cd /repos
 
-    if [ ! -d .repo ]; then
-        repo init -u "${MANIFEST_URL}" -b "${MANIFEST_REVISION}" -m "${MANIFEST_NAME}" -g "${MANIFEST_GROUPS}" --mirror
-    else
-        repo init -u "${MANIFEST_URL}" -b "${MANIFEST_REVISION}" -m "${MANIFEST_NAME}" -g "${MANIFEST_GROUPS}"
+if [ ! -d .repo ]; then
+    repo init -u "${MANIFEST_URL}" -b "${MANIFEST_REVISION}" -m "${MANIFEST_NAME}" -g "${MANIFEST_GROUPS}" --mirror
+else
+    repo init -u "${MANIFEST_URL}" -b "${MANIFEST_REVISION}" -m "${MANIFEST_NAME}" -g "${MANIFEST_GROUPS}"
+fi
+
+repo sync
+
+> cgitrepos
+while read -r line; do
+    line_split=(${line// : / })
+    line_path=${line_split[0]}
+    line_url=${line_split[1]}
+    if ! [[ "${line_path}" =~ ^.*\.git$ ]]; then
+        line_path="${line_path}.git"
     fi
+    echo "repo.url=${line_url}" >> cgitrepos
+    echo "repo.path=/repos/${line_path}" >> cgitrepos
+    echo "" >> cgitrepos
 
-    while true; do
-        repo sync
-
-        > cgitrepos
-        while read -r line; do
-            line_split=(${line// : / })
-            line_path=${line_split[0]}
-            line_url=${line_split[1]}
-            if ! [[ "${line_path}" =~ ^.*\.git$ ]]; then
-                line_path="${line_path}.git"
-            fi
-            echo "repo.url=${line_url}" >> cgitrepos
-            echo "repo.path=/repos/${line_path}" >> cgitrepos
-            echo "" >> cgitrepos
-
-            # Provide an alternative .git URL
-            if ! [[ "${line_url}" =~ ^.*\.git$ ]]; then
-                echo "repo.url=${line_url}.git" >> cgitrepos
-                echo "repo.path=/repos/${line_path}" >> cgitrepos
-                echo "repo.hide=1"  >> cgitrepos
-                echo "" >> cgitrepos
-            fi
-        done <<< "$(repo list)"
-
-        sleep `expr 60 \* ${UPDATE_INTERVAL}`
-    done
-} &
-
-exec /usr/sbin/lighttpd -D -f /etc/lighttpd/lighttpd.conf
+    # Provide an alternative .git URL
+    if ! [[ "${line_url}" =~ ^.*\.git$ ]]; then
+        echo "repo.url=${line_url}.git" >> cgitrepos
+        echo "repo.path=/repos/${line_path}" >> cgitrepos
+        echo "repo.hide=1"  >> cgitrepos
+        echo "" >> cgitrepos
+    fi
+done <<< "$(repo list)"
